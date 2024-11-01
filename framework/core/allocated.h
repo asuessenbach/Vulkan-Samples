eb9fb0520ded5b63b4e6630fa7c877760a8435cd
Bradley Austin Davis <bdavis@saintandreas.org>
2024-11-01 11:49:57 -0700

    Adding documentation to the allocated class hierarchy, base classes edition

--- framework/core/allocated.h
+++ framework/core/allocated.h
@@ -28,10 +28,26 @@ class Device;
 
 namespace allocated
 {
+/**
+ * @brief Retrieves a reference to the VMA allocator singleton.  It will hold a null value except between calls to `init` and `shutdown`.
+ * @return The VMA allocator instance.
+ */
 VmaAllocator &get_memory_allocator();
 
+/**
+ * @brief The non-templatized VMA initializer function, referenced by the template version to smooth
+ * over the differences between the `vkb::Device` and `vkb::core::HPPDevice` classes.
+ * Idempotent, but should be paired with `shutdown`.
+ * @param create_info The VMA allocator create info.
+ */
 void init(const VmaAllocatorCreateInfo &create_info);
 
+/**
+ * @brief Initializes the VMA allocator with the specified device, expressed
+ * as the `vkb` wrapper class, which might be `vkb::Device` or `vkb::core::HPPDevice`.
+ * @tparam DeviceType The type of the device.
+ * @param device The Vulkan device.
+ */
 template <typename DeviceType = vkb::Device>
 void init(const DeviceType &device)
 {
@@ -80,8 +96,38 @@ void init(const DeviceType &device)
 	init(allocator_info);
 }
 
+/**
+ * @brief Retrieves the VMA allocator singleton instance.
+ * @return The VMA allocator instance.
+ */
+VmaAllocator &get_memory_allocator();
+
+/**
+ * @brief Shuts down the VMA allocator and releases all resources.  Idempotent, but should be paired with `init` and attempting to run examples
+ */
 void shutdown();
 
+/**
+ */
+
+/**
+ * @note This class was originally created as a non-templated base class for the `Allocated` class,
+ * so that the implementation could be shunted to a cpp file to avoid triggering a complete rebuild
+ * of the entire project when the implementation changed.  However, since it's now a templated class,
+ * there's no reason to keep it distinct from the `Allocated` class, and this class could be merged into
+ * `Allocated` for a less complex class hierarchy.
+ *
+ * @note Constants used in this documentation in the form of `HOST_COHERENT` are shorthand for
+ * `VK_MEMORY_PROPERTY_HOST_COHERENT_BIT` used for the sake of brevity.
+ *
+ * @brief The base class for all resource which use allocated memory in addition to the raw resource
+ * type.  This project uses the [VMA](https://gpuopen.com/vulkan-memory-allocator/) to handle the low
+ * level details of memory allocation and management, as it hides away many of the messyy details of
+ * memory allocation when a user is first learning Vulkan, but still allows for fine grained control
+ * when a user becomes more experienced and the situation calls for it.
+ *
+ * @tparam bindingType A flag indicating whether this is being used with the C or C++ API
+ */
 template <vkb::BindingType bindingType>
 class AllocatedBase
 {
@@ -103,49 +149,83 @@ class AllocatedBase
 	AllocatedBase &operator=(AllocatedBase &&)      = default;
 
 	/**
-	 * @brief Flushes memory if it is HOST_VISIBLE and not HOST_COHERENT
+	 * @brief Flushes memory if it is NOT `HOST_COHERENT` (which also implies `HOST_VISIBLE`).
+	 * This is a no-op for `HOST_COHERENT` memory.
+	 *
+	 * @param offset The offset into the memory to flush.  Defaults to 0.
+	 * @param size The size of the memory to flush.  Defaults to the entire block of memory.
 	 */
 	void flush(DeviceSizeType offset = 0, DeviceSizeType size = VK_WHOLE_SIZE);
 
-	const uint8_t   *get_data() const;
+	/**
+	 * @brief Retrieves a pointer to the host visible memory as an unsigned byte array.
+	 * @return The pointer to the host visible memory.
+	 * @note This performs no checking that the memory is actually mapped, so it's possible to get a nullptr
+	 */
+	const uint8_t *get_data() const;
+	/**
+	 * @brief Retrieves the raw Vulkan memory object.
+	 * @return The Vulkan memory object.
+	 */
 	DeviceMemoryType get_memory() const;
 
 	/**
-	 * @brief Maps vulkan memory if it isn't already mapped to an host visible address
-	 * @return Pointer to host visible memory
+	 * @brief Maps Vulkan memory if it isn't already mapped to a host visible address. Does northing if the
+	 * allocation is already mapped (including persistently mapped allocations).
+	 * @return Pointer to host visible memory.
 	 */
 	uint8_t *map();
 
 	/**
-	 * @brief Returns true if the memory is mapped, false otherwise
-	 * @return mapping status
+	/**
+	 * @brief Returns true if the memory is mapped (i.e. the object contains a pointer for the mapping).
+	 * This is true for both objects where `map` has been called as well as objects created with persistent
+	 * mapping, where no call to `map` is necessary.
+	 * @return mapping status.
 	 */
 	bool mapped() const;
 
 	/**
-	 * @brief Unmaps vulkan memory from the host visible address
+	 * @brief Unmaps Vulkan memory from the host visible address.  Does nothing if the memory is not mapped or
+	 * if the allocation is persistently mapped.
 	 */
 	void unmap();
 
 	/**
-	 * @brief Copies byte data into the buffer
-	 * @param data The data to copy from
-	 * @param size The amount of bytes to copy
-	 * @param offset The offset to start the copying into the mapped data
+	 * @brief Copies the specified unsigned byte data into the mapped memory region.
+	 * @note For non-persistently mapped memory, this function will call the `map` and `unmap` methods and SHOULD NOT
+	 * be used if the user intends to make multiple updates to the memory region.  In that case, the user should call
+	 * `map` once, make all the updates against the pointer returned by `get_data`, and then call `unmap`.  This may
+	 * be a poor design choice as it creates a side effect of using the method (that mapped memory will
+	 * unexpectedly be unmapped), but it is the current design of the method and changing it would be burdensome.
+	 * Refactoring could be eased by creating a new method with a more explicit name, and then removing this method
+	 * entirely.
+	 *
+	 * @param data The data to copy from.
+	 * @param size The amount of bytes to copy.
+	 * @param offset The offset to start the copying into the mapped data. Defaults to 0.
 	 */
 	size_t update(const uint8_t *data, size_t size, size_t offset = 0);
 
 	/**
-	 * @brief Converts any non byte data into bytes and then updates the buffer
-	 * @param data The data to copy from
-	 * @param size The amount of bytes to copy
-	 * @param offset The offset to start the copying into the mapped data
+	 * @brief Converts any non-byte data into bytes and then updates the buffer.  This allows the user to pass
+	 * arbitrary structure pointers to the update method, which will then be copied into the buffer as bytes.
+	 * @param data The data to copy from.
+	 * @param size The amount of bytes to copy.
+	 * @param offset The offset to start the copying into the mapped data. Defaults to 0.
 	 */
 	size_t update(void const *data, size_t size, size_t offset = 0);
 
 	/**
-	 * @todo Use the vk::ArrayBuffer class to collapse some of these templates
-	 * @brief Copies a vector of items into the buffer
+	 * @todo Use the vk::ArrayBuffer class to replace these multiple templated methods.  To avoid ambiguity
+	 * with the non-templated version of the method, the new `vk::ArrayBuffer` version should be called `updateTyped`
+	 * or something similar.
+	 * @brief Copies a vector of items into the buffer.  This is a convenience method that allows the user to
+	 * pass a vector of items to the update method, which will then be copied into the buffer as bytes.
+	 *
+	 * This function DOES NOT automatically manage adhering to the alignment requirements of the items being copied,
+	 * for instance the `minUniformBufferOffsetAlignment` property of the [device](https://vulkan.gpuinfo.org/displaydevicelimit.php?name=minUniformBufferOffsetAlignment&platform=all).
+	 * If the data needs to be aligned on something other than `sizeof(T)`, the user must manage that themselves.
 	 * @param data The data vector to upload
 	 * @param offset The offset to start the copying into the mapped data
 	 */
@@ -155,6 +235,13 @@ class AllocatedBase
 		return update(data.data(), data.size() * sizeof(T), offset);
 	}
 
+	/**
+	 * @todo Use the vk::ArrayBuffer class to replace these multiple templated methods.
+	 * @brief Another convenience method, similar to the vector update method, but for std::array. The same caveats apply.
+	 * @param data The data vector to upload
+	 * @param offset The offset to start the copying into the mapped data
+	 * @see update(std::vector<T> const &data, size_t offset = 0)
+	 */
 	template <typename T, size_t N>
 	size_t update(std::array<T, N> const &data, size_t offset = 0)
 	{
@@ -162,7 +249,10 @@ class AllocatedBase
 	}
 
 	/**
-	 * @brief Copies an object as byte data into the buffer
+	 * @todo Use the vk::ArrayBuffer class to replace these multiple templated methods.
+	 * @brief Copies an object as byte data into the buffer.  This is a convenience method that allows the user to
+	 * pass an object to the update method, which will then be copied into the buffer as bytes.  The name difference
+	 * is to avoid amibuity with the `update` method signatures (including the non-templated version)
 	 * @param object The object to convert into byte data
 	 * @param offset The offset to start the copying into the mapped data
 	 */
@@ -173,12 +263,49 @@ class AllocatedBase
 	}
 
   protected:
-	virtual void             post_create(VmaAllocationInfo const &allocation_info);
-	void                     clear();
+	/**
+	 * @brief Internal method to actually create the buffer, allocate the memory and bind them.
+	 * Should only be called from the `Buffer` derived class.
+	 *
+	 * Present in this common base class in order to allow the internal state members to remain `private`
+	 * instead of `protected`, and because it (mostly) isolates interaction with the VMA to a single class
+	 */
 	[[nodiscard]] BufferType create_buffer(BufferCreateInfoType const &create_info);
-	[[nodiscard]] ImageType  create_image(ImageCreateInfoType const &create_info);
-	void                     destroy_buffer(BufferType buffer);
-	void                     destroy_image(ImageType image);
+	/**
+	 * @brief Internal method to actually create the image, allocate the memory and bind them.
+	 * Should only be called from the `Image` derived class.
+	 *
+	 * Present in this common base class in order to allow the internal state members to remain `private`
+	 * instead of `protected`, and because it (mostly) isolates interaction with the VMA to a single class
+	 */
+	[[nodiscard]] ImageType create_image(ImageCreateInfoType const &create_info);
+	/**
+	 * @brief The post_create method is called after the creation of a buffer or image to store the allocation info internally.  Derived classes
+	 * could in theory override this to ensure any post-allocation operations are performed, but the base class should always be called to ensure
+	 * the allocation info is stored.
+	 * Should only be called in the corresping `create_xxx` methods.
+	 */
+	virtual void post_create(VmaAllocationInfo const &allocation_info);
+
+	/**
+	 * @brief Internal method to actually destroy the buffer and release the allocated memory.  Should
+	 * only be called from the `Buffer` derived class.
+	 * Present in this common base class in order to allow the internal state members to remain `private`
+	 * instead of `protected`, and because it (mostly) isolates interaction with the VMA to a single class
+	 */
+	void destroy_buffer(BufferType buffer);
+	/**
+	 * @brief Internal method to actually destroy the image and release the allocated memory.  Should
+	 * only be called from the `Image` derived class.
+	 * Present in this common base class in order to allow the internal state members to remain `private`
+	 * instead of `protected`, and because it (mostly) isolates interaction with the VMA to a single class
+	 */
+	void destroy_image(ImageType image);
+	/**
+	 * @brief Clears the internal state.  Can be overridden by derived classes to perform additional cleanup of members.
+	 * Should only be called in the corresping `destroy_xxx` methods.
+	 */
+	void clear();
 
   private:
 	vk::Buffer create_buffer_impl(vk::BufferCreateInfo const &create_info);
@@ -440,6 +567,15 @@ inline size_t AllocatedBase<bindingType>::update(void const *data, size_t size, 
 	return update(reinterpret_cast<const uint8_t *>(data), size, offset);
 }
 
+/**
+ * @note Most of the common functionality is implemented in the `AllocatedBase` class, which was preveiously
+ * not templated and had its implementation shunted to a cpp file to avoid triggering a complete rebuild of the
+ * project with every change.  However, these two classes should now probably be merged.
+ * @brief The `Allocated` class is serves as a base class for wrappers around Vulkan that require memory allocation
+ * (`VkImage` and `VkBuffer`).  This class mostly ensures proper behavior for a RAII pattern, preventing double-release by
+ * preventing copy assignment and copy construction in favor of move semantics, as well as preventing default construction
+ * in favor of explicit construction with a pre-existing handle or a populated create info struct.
+ */
 template <vkb::BindingType bindingType, typename HandleType>
 class Allocated : public vkb::core::VulkanResource<bindingType, HandleType>, public AllocatedBase<bindingType>
 {
@@ -454,10 +590,17 @@ class Allocated : public vkb::core::VulkanResource<bindingType, HandleType>, pub
 	Allocated &operator=(Allocated const &other) = delete;
 	Allocated &operator=(Allocated &&other)      = default;
 
-	// Import the base class constructors
+	/**
+	 * @tparam Args Additional constructor arguments needed for the derived class.  Typically a `VkImageCreateInfo` or `VkBufferCreateInfo` struct.
+	 * @param allocation_create_info All of the non-resource-specific information needed by the VMA to allocate the memory.
+	 */
 	template <typename... Args>
 	Allocated(const VmaAllocationCreateInfo &allocation_create_info, Args &&...args);
 
+	/**
+	 * @brief This constructor is used when the handle is already created, and the user wants to wrap it in an `Allocated` object.
+	 * @note I don't recall why this was necessary, and it might be something that should be either deprecated or removed.
+	 */
 	Allocated(HandleType handle, DeviceType *device_ = nullptr);
 
 	Allocated(Allocated &&other) noexcept;
